bf843e8135ff947118d19767f41deeb4ed1fa150
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-06-18 23:35:25 +0530

    created auth routes

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const express = require("express");
 const connectDB = require('./mongoDB/db');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 app.use(express.json());
 
 connectDB();
+
+app.use('/auth', authRoutes);
 
 app.listen(3000, () => {
     console.log(`Server running on port` + " " + 3000);
