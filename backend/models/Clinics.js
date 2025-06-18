97674ee6898fd7312623dfab34adf2d9328eb569
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-06-18 22:33:31 +0530

    created clinic schema

---
+++ backend/models/Clinics.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const clinicSchema = new Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    address: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    contactNumber:{
+        type: Number,
+        required: true
+    },
+    adminId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Clinic', clinicSchema);
