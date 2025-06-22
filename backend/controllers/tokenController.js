2a3a0b03561f9523e0b2989d2a8ed87b157cb6df
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-06-22 22:33:06 +0530

    created token apis

--- backend/controllers/tokenController.js
+++ backend/controllers/tokenController.js
@@ -51,3 +51,60 @@ exports.bookToken = async (req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
 };
+
+
+exports.getTodayTokensForClinic = async (req, res) => {
+  const clinicId = req.params.clinicId;
+
+  try {
+    const startOfDay = new Date();
+    startOfDay.setHours(0, 0, 0, 0);
+
+    const tokens = await Token.find({
+      clinicId,
+      bookedAt: { $gte: startOfDay }
+    })
+      .sort({ tokenNumber: 1 })
+      .populate('patientId', 'name email phone'); // to show patient info
+
+    res.status(200).json({ tokens });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Failed to load tokens' });
+  }
+};
+
+exports.callToken = async (req, res) => {
+  try {
+    const token = await Token.findById(req.params.id);
+    if (!token || token.status !== 'waiting') {
+      return res.status(400).json({ message: 'Invalid token or already called' });
+    }
+
+    token.status = 'called';
+    token.calledAt = new Date();
+    await token.save();
+
+    res.status(200).json({ message: 'Token called', token });
+  } catch (err) {
+    res.status(500).json({ message: 'Error calling token' });
+  }
+};
+
+exports.completeToken = async (req, res) => {
+  try {
+    const token = await Token.findById(req.params.id);
+    if (!token || token.status !== 'called') {
+      return res.status(400).json({ message: 'Token must be called before completing' });
+    }
+
+    token.status = 'completed';
+    token.completedAt = new Date();
+    await token.save();
+
+    res.status(200).json({ message: 'Token marked as completed', token });
+  } catch (err) {
+    res.status(500).json({ message: 'Error completing token' });
+  }
+};
+
--- backend/routes/tokenRoutes.js
+++ backend/routes/tokenRoutes.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const protect = require('../middleware/protect');
-const { bookToken } = require('../controllers/tokenController');
+const {
+  bookToken,
+  getTodayTokensForClinic,
+  callToken,
+  completeToken
+} = require('../controllers/tokenController');
+
 
 router.post('/book', protect, bookToken);
+router.get('/admin/clinic/:clinicId/tokens', protect, getTodayTokensForClinic);
+router.put('/:id/call', protect, callToken);
+router.put('/:id/complete', protect, completeToken);
+
+
 
 module.exports = router;
