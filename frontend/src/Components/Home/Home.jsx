dfc5e2ce36b905331493fb7e5fa1c32e8eef89e3
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-07-21 15:33:06 +0530

    Created Hero section in home page

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import './Home.css';
+import img from '../../public/backlogo.jpg';
 
 const Home = () => {
   return (
     <div id="home" className='home'>
-      Home
-    </div>
+      <div className='home-quote'>
+        <h2> " <b style={{color: "#00b4d8"}}>Skip</b> the waiting room. Your <b style={{color: "#00b4d8"}}>health</b> deserves better."</h2>
+        <img  src={img} className='home-img'/>
+      </div >
+
+      <div>
+        DOC PROFILE
+      </div>
+    </div >
   )
 }
 
