fae4ce4c9df0b1dee1d016a2df71bda6a8404b41
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-07-22 14:58:55 +0530

    created and styled patient page

--- frontend/src/Components/PatientQueue/PatientQueue.jsx
+++ frontend/src/Components/PatientQueue/PatientQueue.jsx
@@ -1,17 +1,19 @@
 import Table from 'react-bootstrap/Table';
+import './PatientQueue.css'
 
 function PatientQueue() {
     return (
-        <div className=''>
+        <div className='patient-container'>
             <h2>PATIENTS MANAGER</h2>
-            <Table responsive="sm">
+            <Table responsive="sm" hover className='patient-table'>
                 <thead>
                     <tr>
                         <th>#</th>
                         <th>Patient Name</th>
                         <th>Token Number</th>
                         <th>Status</th>
-                       
+                        <th>Mark</th>
+
                     </tr>
                 </thead>
                 <tbody>
@@ -20,20 +22,119 @@ function PatientQueue() {
                         <td>Ayush</td>
                         <td>1</td>
                         <td>Called</td>
-                        
+                        <td>Completed | Called | Waiting</td>
+
                     </tr>
                     <tr>
                         <td>2</td>
                         <td>Tarun</td>
                         <td>2</td>
                         <td>Waiting</td>
-                        
+                        <td>Completed | Called | Waiting</td>
+
                     </tr>
                     <tr>
                         <td>3</td>
                         <td>Vinayak</td>
                         <td>3</td>
                         <td>Waiting</td>
+                        <td>Completed | Called | Waiting</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Ayush</td>
+                        <td>1</td>
+                        <td>Called</td>
+                        <td>Completed | Called | Waiting</td>
+
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Tarun</td>
+                        <td>2</td>
+                        <td>Waiting</td>
+                        <td>Completed | Called | Waiting</td>
+
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Vinayak</td>
+                        <td>3</td>
+                        <td>Waiting</td>
+                        <td>Completed | Called | Waiting</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Ayush</td>
+                        <td>1</td>
+                        <td>Called</td>
+                        <td>Completed | Called | Waiting</td>
+
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Tarun</td>
+                        <td>2</td>
+                        <td>Waiting</td>
+                        <td>Completed | Called | Waiting</td>
+
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Vinayak</td>
+                        <td>3</td>
+                        <td>Waiting</td>
+                        <td>Completed | Called | Waiting</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Ayush</td>
+                        <td>1</td>
+                        <td>Called</td>
+                        <td>Completed | Called | Waiting</td>
+
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Tarun</td>
+                        <td>2</td>
+                        <td>Waiting</td>
+                        <td>Completed | Called | Waiting</td>
+
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Vinayak</td>
+                        <td>3</td>
+                        <td>Waiting</td>
+                        <td>Completed | Called | Waiting</td>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Ayush</td>
+                        <td>1</td>
+                        <td>Called</td>
+                        <td>Completed | Called | Waiting</td>
+
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Tarun</td>
+                        <td>2</td>
+                        <td>Waiting</td>
+                        <td>Completed | Called | Waiting</td>
+
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Vinayak</td>
+                        <td>3</td>
+                        <td>Waiting</td>
+                        <td>Completed | Called | Waiting</td>
                     </tr>
                 </tbody>
             </Table>
