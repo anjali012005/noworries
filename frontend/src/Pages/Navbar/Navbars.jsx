6b4b0856594f996233c13c921cf96d388d05c2f0
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-07-24 13:56:55 +0530

    Created routes for signup and login page

--- frontend/src/Pages/Navbar/Navbars.jsx
+++ frontend/src/Pages/Navbar/Navbars.jsx
@@ -9,12 +9,12 @@ const Navbars = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container className='nav-container'>
-        <div className='nav-name'><Navbar.Brand href="/" style={{color: "#00b4d8",  fontWeight: 800}}>noWorries</Navbar.Brand></div>
+        <div className='nav-name'><Navbar.Brand href="/home" style={{color: "#00b4d8",  fontWeight: 800}}>noWorries</Navbar.Brand></div>
         <div>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto nav-items">
-              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/home">Home</Nav.Link>
               <Nav.Link href="/queue">Queue</Nav.Link>
               <Nav.Link href="#link">Connect With Us</Nav.Link>
               <NavDropdown title="Signed In as: User" id="basic-nav-dropdown"> 
