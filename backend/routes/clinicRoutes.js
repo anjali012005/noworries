c9a63e4a56b2df0904d9cd32306f7f54e7b83eca
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-09-08 21:18:37 +0530

    Reinitialized web

--- backend/routes/clinicRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const protect = require('../middleware/protect');
-const { createClinic } = require('../controllers/clinicController');
-
-router.post('/create', protect, createClinic);
-
-module.exports = router;
