f4682a8ee4a51c3908d20a101c6b394db6b5f44a
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-09-10 23:52:08 +0530

    All setup done now!

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="">
-      {/* <h1 className="text-3xl underline ">HOME</h1> */}
+      <h1 className="text-3xl underline ">HOME</h1>
       <Button>Click me</Button>
     </div>
   );
