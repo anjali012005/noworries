f3310292bfa972ebbe7a634a31594cbbedd1732a
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-09-14 23:59:23 +0530

    Building Home section!

---
+++ components/CustomFormField.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Control } from "react-hook-form"
+import { FormFieldType } from "./forms/PatientForm"
+
+interface CustomProps {
+    control : Control<any>,
+    fieldType : FormFieldType
+}
+
+const CustomFormField = ({control, fieldType, name}: CustomProps) => {
+    return (
+        <FormField
+            control={control}
+            name={name}
+            render={({ field }) => (
+                <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                        <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                        This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                </FormItem>
+            )}
+        />
+    )
+}
+
+export default CustomFormField
