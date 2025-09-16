75cd636b2fceb0ea5e5895207dc5750fbbbc2809
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-09-16 18:30:59 +0530

    Created SignUp Page UI

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -13,6 +13,10 @@ import { Input } from "@/components/ui/input"
 import { Control } from "react-hook-form"
 import { FormFieldType } from "./forms/PatientForm"
 import Image from "next/image"
+import 'react-phone-number-input/style.css'
+import PhoneInput, { Value } from 'react-phone-number-input'
+import { E164Number } from "libphonenumber-js";
+
 
 interface CustomProps {
     control: Control<any>,
@@ -30,12 +34,12 @@ interface CustomProps {
 }
 
 const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
-    const {fieldType, iconSrc, iconAlt, placeholder} = props;
+    const { fieldType, iconSrc, iconAlt, placeholder } = props;
 
     switch (fieldType) {
         case FormFieldType.INPUT:
             return (
-                <div className="flex rounded-md border border-dark-500 bg-dark-400">
+                <div className="flex rounded-md border border-dark-500 bg-dark-400 p-1">
                     {iconSrc && (
                         <Image
                             src={iconSrc}
@@ -47,21 +51,34 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
                     )}
                     <FormControl>
                         <Input
-                        placeholder={placeholder}
-                        {...field}
-                        className="shad-input-border-0" 
+                            placeholder={placeholder}
+                            {...field}
+                            className="shad-input-border-0 focus-visible:ring-0 focus-visible:ring-offset-0
+                        focus:outline-none 
+                        focus-visible:outline-none 
+     
+                        focus:border-transparent 
+                        focus:shadow-none"
                         />
                     </FormControl>
                 </div>
             )
-        
-            case FormFieldType.PHONE_INPUT:
-                return (
-                    <FormControl>
-                        {/* <PhoneInput /> */}
-                    </FormControl>
-                )
-            default:
+
+        case FormFieldType.PHONE_INPUT:
+            return (
+                <FormControl>
+                    <PhoneInput
+                        defaultCountry="IN"
+                        placeholder={placeholder}
+                        international
+                        withCountryCallingCode
+                        value={field.value as E164Number | undefined}
+                        onChange={field.onChange}
+                        className="input-phone"
+                    />
+                </FormControl>
+            )
+        default:
             break;
     }
 }
